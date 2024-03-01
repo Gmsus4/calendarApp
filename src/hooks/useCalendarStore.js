594972c2523a13f2addb23b224af840b34f2a67b
Fernando <jesusfgm45@gmail.com>
2024-03-01 11:28:19 -0600

    Cargar un evento en el modal

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,12 +1,19 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { onSetActiveEvent } from "../store/calendar/calendarSlice";
 
 export const useCalendarStore = () => {
+   const dispatch = useDispatch(); 
    const { events, activeEvent } = useSelector(state => state.calendar);
+
+   const setActiveEvent = (calendarEvent) => {
+      dispatch(onSetActiveEvent(calendarEvent))
+   }
   return {
     //* Propiedades
     activeEvent,
     events, 
 
     //*Metodos
+    setActiveEvent
   }
 }
