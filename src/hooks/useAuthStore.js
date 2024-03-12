4c40eb6fd896c75e9aa3730cfb20c3eeb93bf294
Fernando <jesusfgm45@gmail.com>
2024-03-12 12:37:50 -0600

    Mantener el estado de autenticación

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -38,6 +38,20 @@ export const useAuthStore = () => {
         }
     }
 
+    const checkAuthToken = async() => {
+        const token = localStorage.getItem('token');
+        if(!token) return dispatch(onLogout());
+        try {
+            const { data } = await calendarApi.get('auth/renew');
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({name: data.name, uid: data.uid}));
+        } catch (error) {
+            localStorage.clear();
+            dispatch(onLogout());
+        }
+    }
+
     return {
         //*Propiedades
         status,
@@ -45,6 +59,7 @@ export const useAuthStore = () => {
         errorMessage,
 
         //*Metodos
+        checkAuthToken,
         startLogin,
         startRegister
     }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
+import { useAuthStore } from "../hooks";
+import { useEffect } from "react";
 
 export const AppRouter = () => {
-    const authStatus = 'not-autenticated' // 'authenticated'; // 'not-autenticated';
+    // const authStatus = 'not-not-authenticated' // 'authenticated'; // 'not-autenticated';
+    const { status, checkAuthToken } = useAuthStore();
+    
+    useEffect(() => {
+      checkAuthToken();
+    }, [])
+
+    if(status === 'checking'){
+      return (
+        <h3>Cargando...</h3>
+      )
+    }
 
   return (
     <Routes>
         { 
-            (authStatus === 'not-autenticated')
+            (status === 'not-authenticated')
                 ? <Route path="/auth/*" element={<LoginPage />}/>
                 : <Route path="/*" element={<CalendarPage />}/>
         }
