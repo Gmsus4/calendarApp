d64f706d2cbb28006f540ea41586cfb9481c17b5
Fernando <jesusfgm45@gmail.com>
2024-03-12 11:27:38 -0600

    Creación de un nuevo usuario

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -22,6 +22,22 @@ export const useAuthStore = () => {
         }
     }
 
+    const startRegister = async({email, password, name}) => {
+        dispatch(onChecking()); //Revisando estado de carga
+
+        try {
+            const { data } = await calendarApi.post('/auth/new', {email, password, name});
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({name: data.name, uid: data.uid}));
+        } catch (error) {
+            dispatch(onLogout(error.response.data?.msg || '--'));
+            setTimeout(() => {
+                dispatch(clearErrorMessage());
+            }, 10);
+        }
+    }
+
     return {
         //*Propiedades
         status,
@@ -29,6 +45,7 @@ export const useAuthStore = () => {
         errorMessage,
 
         //*Metodos
-        startLogin
+        startLogin,
+        startRegister
     }
 }
