f8e140cf83e73a93a0dbd054925a7050c2032f11
Fernando <jesusfgm45@gmail.com>
2024-03-12 19:40:01 -0600

    getEnvVariables modificado

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,7 +1,8 @@
 export const getEnvVariables = () => {
-    import.meta.env; //Lo vimos en el testing del journal
+    // import.meta.env; //Lo vimos en el testing del journal
 
     return {
-        ...import.meta.env
+        // ...import.meta.env
+        VITE_API_URL: import.meta.env.VITE_API_URL,
     }
 }
